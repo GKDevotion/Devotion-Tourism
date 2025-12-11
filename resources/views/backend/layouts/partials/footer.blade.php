1e8a8fe98339a8385f81f22fab6def6221f31c02
hardik prajapati <hardikprajapati8104@gmail.com>
2025-12-11 16:10:45 +0530

    initial commit

--- resources/views/backend/layouts/partials/footer.blade.php
+++
@@ -1,8 +0,0 @@
-
-<!-- footer area start-->
-<footer>
-    <div class="footer-area">
-        <p>© Copyright {{date('Y')}}. All right reserved. by <a href="https://devotiongroup.org" target="_blank">Devotion Group</a>.</p>
-    </div>
-</footer>
-<!-- footer area end-->
--- resources/views/backend/layouts/partials/side-bar-logo.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="sidebar-header">
-    <div class="logo">
-        <a href="{{route('admin.dashboard.index')}}">
-            <img src="{{url(getConfigurationfield('APP_LOGO'))}}" />
-        </a>
-    </div>
-</div>
